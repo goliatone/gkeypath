bc053c40856fafd50837c29524b296bd80c6ed03
goliatone <burgosemi@gmail.com>
2014-09-14 00:50:21 -0400

    EXAMPLE: Update...

--- examples/main.js
+++ examples/main.js
@@ -6,7 +6,34 @@ requirejs.config({
     }
 });
 
-define(['keypath'], function(keypath) {
-    console.log('Loading');
+define(['keypath'], function(Keypath) {
+    console.log('Loading', Keypath);
 
+    var user = {
+        name: 'Peperone',
+        age: 23,
+        address: {
+            street: 'Dekalb',
+            zip: 1111
+        },
+        pet: {
+            name: 'Maui'
+        },
+        car: {
+            brand: {
+                model: 'None'
+            }
+        }
+    };
+
+    Keypath.wrap(user, 'keypath');
+    console.log(user.keypath.get('car.brand.model'));
+
+    var userKeypath = Keypath.wrap(user);
+    console.log(userKeypath.get('car.brand.model'));
+
+    console.log(Keypath.get(user, 'car.brand.model'));
+
+
+    window.Keypath = Keypath;
 });
