17c89e29e050aeb8dcc197a576b1f7b4dfecd5d7
goliatone <burgosemi@gmail.com>
2014-09-30 19:28:10 -0400

    SPEC: GKeypath tests for Wrapper. Refactoring.

--- test/spec/gkeypath-spec.js
+++ test/spec/gkeypath-spec.js
@@ -1,12 +1,12 @@
 /*global define:true, describe:true , it:true , expect:true,
 beforeEach:true, sinon:true, spyOn:true , expect:true */
 /* jshint strict: false */
-define(['keypath'], function(keypath) {
+define(['keypath'], function(Keypath) {
 
-    describe('just checking', function() {
+    describe('Keypath', function() {
 
-        it('keypath should be loaded', function() {
-            expect(keypath).toBeTruthy();
+        it('should be loaded', function() {
+            expect(Keypath).toBeTruthy();
         });
 
         beforeEach(function() {
@@ -14,23 +14,23 @@ define(['keypath'], function(keypath) {
         });
 
         it('should have a set method', function() {
-            expect(keypath).toHaveMethods('set');
+            expect(Keypath).toHaveMethods('set');
         });
 
         it('should have a get method', function() {
-            expect(keypath).toHaveMethods('get');
+            expect(Keypath).toHaveMethods('get');
         });
 
         it('should have a has method', function() {
-            expect(keypath).toHaveMethods('has');
+            expect(Keypath).toHaveMethods('has');
         });
 
-        it('set should handle keypaths', function() {
+        it('set should handle Keypaths', function() {
             var data = {},
                 path = 'path.to.property',
                 value = 'newValue';
 
-            keypath.set(data, path, value);
+            Keypath.set(data, path, value);
             expect(data.path.to.property).toEqual(value);
         });
 
@@ -38,11 +38,11 @@ define(['keypath'], function(keypath) {
             var path = 'path.to.property',
                 value = 'newValue';
 
-            var data = keypath.set(null, path, value);
+            var data = Keypath.set(null, path, value);
             expect(data).toBeFalsy();
         });
 
-        it('get should retrieve values using keypaths', function() {
+        it('get should retrieve values using Keypaths', function() {
             var data = {
                 path: {
                     to: {
@@ -52,7 +52,7 @@ define(['keypath'], function(keypath) {
             }, path = 'path.to.property',
                 value = 'newValue';
 
-            expect(keypath.get(data, path)).toEqual(value);
+            expect(Keypath.get(data, path)).toEqual(value);
         });
 
         it('get should return provided default value if path does not exist', function() {
@@ -60,7 +60,7 @@ define(['keypath'], function(keypath) {
                 path = 'path.to.property',
                 defValue = 'newValue';
 
-            expect(keypath.get(data, path, defValue)).toEqual(defValue);
+            expect(Keypath.get(data, path, defValue)).toEqual(defValue);
         });
 
         it('has should return boolean value indicating if target has path', function() {
@@ -74,8 +74,34 @@ define(['keypath'], function(keypath) {
                 wrongPath = 'made.up.path.here',
                 value = 'newValue';
 
-            expect(keypath.has(data, path)).toBeTruthy();
-            expect(keypath.has(data, wrongPath)).toBeFalsy();
+            expect(Keypath.has(data, path)).toBeTruthy();
+            expect(Keypath.has(data, wrongPath)).toBeFalsy();
+        });
+
+        it('private method _get returns value from property or getter', function(){
+            var expected = 'value',
+                source = {
+                    prop:expected,
+                    getter:function(){
+                        return expected;
+                    }
+                };
+            expect(Keypath._get(source.prop)).toEqual(expected);
+            expect(Keypath._get(source.getter)).toEqual(expected);
+        });
+
+        it('private method _set sets value to a property or getter', function(){
+            var expected = false,
+                source = {
+                    prop:false,
+                    setter:function(value){
+                        expected = value;
+                    }
+                };
+            Keypath._set(source, 'prop', true)
+            expect(source.prop).toEqual(true);
+            Keypath._set(source, 'setter', true)
+            expect(expected).toEqual(true);
         });
     });
 });
