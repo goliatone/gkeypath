ba03f55a405c425334e3f61f35c6051b4af82ebe
goliatone <burgosemi@gmail.com>
2014-09-30 20:02:06 -0400

    EXAMPLE: Update example, added wrap

--- examples/main.js
+++ examples/main.js
@@ -9,30 +9,43 @@ requirejs.config({
 define(['keypath'], function(Keypath) {
     console.log('Loading', Keypath);
 
-    var user = {
-        name: 'Peperone',
-        age: 23,
-        address: {
-            street: 'Dekalb',
-            zip: 1111
-        },
-        pet: {
-            name: 'Maui'
-        },
-        car: {
-            brand: {
-                model: 'None'
+    function makeUser(){
+        var user = {
+            name: 'Peperone',
+            age: 23,
+            address: {
+                street: 'Dekalb',
+                zip: 1111
+            },
+            pet: {
+                name: 'Maui'
+            },
+            car: {
+                brand: {
+                    model: 'AModel'
+                }
             }
-        }
-    };
+        };
+        return user;
+    }
 
+    var user = makeUser();
     Keypath.wrap(user, 'keypath');
     console.log(user.keypath.get('car.brand.model'));
 
+    user = makeUser();
     var userKeypath = Keypath.wrap(user);
     console.log(userKeypath.get('car.brand.model'));
-
     console.log(Keypath.get(user, 'car.brand.model'));
+
+    var inject = function(target, wrapper){
+        target._k = wrapper;
+    };
+
+    user = makeUser();
+    Keypath.wrap(user, inject);
+
+    console.log(user._k.get('car.brand.model'));
 
 
     window.Keypath = Keypath;
