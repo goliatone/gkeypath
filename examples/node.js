b681101c0c8c7183cf2d9fe9e5eb24f968d8bca0
goliatone <burgosemi@gmail.com>
2016-07-25 15:22:45 -0400

    examples: nested wrapping

--- examples/node.js
+++ examples/node.js
@@ -1,9 +1,17 @@
 var Keypath = require('..');
-var config = {bar:{baz:'fiz'}};
+var config = {
+    bar:{
+        baz:'fiz'
+    }
+};
 
 config = Keypath.wrap(config);
 
-console.log(config.bar.baz) //fiz
+console.log(config.bar.baz); //fiz
 console.log(config.has('bar.baz'));
 console.log(config.get('bar.baz'));
 console.log(config.get('bar.boooo', 'jejeje'));
+
+
+console.log('wrapping', config.bar.get('baz'));
+console.log('nested wrapping', config.get('bar').get('baz'));
