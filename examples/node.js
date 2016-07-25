50b665b74762869b5e0635ef64b8f8777f76962c
goliatone <burgosemi@gmail.com>
2016-07-25 16:16:10 -0400

    update examples

--- examples/node.js
+++ examples/node.js
@@ -1,17 +1,39 @@
 var Keypath = require('..');
 var config = {
-    bar:{
-        baz:'fiz'
+    db:{
+        connection:{
+            host: 'locahost',
+            port: 9090,
+            user: 'goliatone',
+            auth: 'admin'
+        }
+    },
+    amqp:{
+        client:{
+            url: 'amqp://localhost:5432/aladflad/'
+        }
     }
 };
 
 config = Keypath.wrap(config);
 
-console.log(config.bar.baz); //fiz
-console.log(config.has('bar.baz'));
-console.log(config.get('bar.baz'));
-console.log(config.get('bar.boooo', 'jejeje'));
+console.log('Something here... %s', config.amqp); //amqp://localhost:5432/aladflad/
+// console.log(config.amqp.client); //amqp://localhost:5432/aladflad/
+// console.log(config.amqp.client.url); //amqp://localhost:5432/aladflad/
+//
+//
+// console.log(config.has('amqp.client.url')); //true
+// console.log(config.get('amqp.client.url')); //amqp://localhost:5432/aladflad/
+// console.log(config.get('amqp.exchange', 'default-exchange')); //default-exchange
+//
+//
+// console.log('wrapping', config.db.get('connection').host);
+// console.log('nested wrapping', config.get('db').get('connection.host'));
+// console.log('nested wrapping', config.get('db').get('connection').get('host'));
 
 
-console.log('wrapping', config.bar.get('baz'));
-console.log('nested wrapping', config.get('bar').get('baz'));
+
+
+
+// console.log('= wrapping', config.db.get('connection').host);
+// console.log(config.get('bar').get('baz'));
