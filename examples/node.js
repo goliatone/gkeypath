9a82d73b15a5b9154eeddebee64507f2eac7e5b3
goliatone <burgosemi@gmail.com>
2016-10-06 18:38:23 -0400

    update example

--- examples/node.js
+++ examples/node.js
@@ -1,5 +1,12 @@
 var Keypath = require('..');
-var config = {bar:{baz:'fiz'}};
+var config = {
+    bar:{
+        baz:'fiz',
+        test: function(){
+            return 'test';
+        }
+    }
+};
 
 config = Keypath.wrap(config);
 
@@ -7,3 +14,4 @@ console.log(config.bar.baz) //fiz
 console.log(config.has('bar.baz'));
 console.log(config.get('bar.baz'));
 console.log(config.get('bar.boooo', 'jejeje'));
+console.log(config.bar.test());
