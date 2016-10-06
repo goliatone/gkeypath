95ca323c2f3d46877709a9f425b9ee37e34084a1
goliatone <burgosemi@gmail.com>
2016-10-06 18:47:13 -0400

    examples: update node

--- examples/node.js
+++ examples/node.js
@@ -8,10 +8,11 @@ var config = {
     }
 };
 
-config = Keypath.wrap(config);
+config = Keypath.wrap(config, undefined, 'data');
 
 console.log(config.bar.baz) //fiz
 console.log(config.has('bar.baz'));
 console.log(config.get('bar.baz'));
 console.log(config.get('bar.boooo', 'jejeje'));
 console.log(config.bar.test());
+console.log(config.data);
