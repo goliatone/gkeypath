1dc12c5ea38542576ca2ee2670b830332e0b3205
goliatone <burgosemi@gmail.com>
2018-08-27 23:05:02 +0200

    update exmpale ;

--- examples/node.js
+++ examples/node.js
@@ -1,5 +1,12 @@
 var Keypath = require('..');
+
+function noop(){
+  console.log('HERE WE ARE AGAIN');
+  return 'kaka';
+}
+
 var config = {
+    '2928b3f4-8b8f-415f-bd80-263e299df658': noop,
     bar:{
         baz:'fiz',
         test: function(){
@@ -10,9 +17,13 @@ var config = {
 
 config = Keypath.wrap(config, undefined, 'data');
 
-console.log(config.bar.baz) //fiz
+/*console.log(config.bar.baz) //fiz
 console.log(config.has('bar.baz'));
 console.log(config.get('bar.baz'));
 console.log(config.get('bar.boooo', 'jejeje'));
 console.log(config.bar.test());
 console.log(config.data);
+*/
+
+console.log(Keypath.get(config, '2928b3f4-8b8f-415f-bd80-263e299df658'));
+console.log(config.get('2928b3f4-8b8f-415f-bd80-263e299df658'));
